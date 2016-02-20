8912da6cc22b2ca0f7424ae54f6ce57228d2e169
Kyle Oba <koba@mudphone.com>
2016-02-20 11:55:35 -1000

    Add machine language multiplier

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@2
+M=0
+
+@0
+D=M
+@END
+D;JEQ
+
+(LOOP)
+  @1
+  D=M
+  @END
+  D;JEQ
+
+  @2
+  D=M
+  @0
+  D=D+M
+  @2
+  M=D
+
+  @1
+  M=M-1
+
+  @LOOP
+  0;JMP
+(END)
