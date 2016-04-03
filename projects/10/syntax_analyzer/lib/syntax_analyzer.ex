bb212b83a48a5d20a582c2cfa12013fee1059756
Kyle Oba <koba@mudphone.com>
2016-04-03 11:28:28 -1000

    Add output to source dir

--- projects/10/syntax_analyzer/lib/syntax_analyzer.ex
+++ projects/10/syntax_analyzer/lib/syntax_analyzer.ex
@@ -16,39 +16,33 @@ defmodule SyntaxAnalyzer do
     |> LineReader.read_lines()
   end
 
-  def tokenize_xml(file_name) do
-    x = translate_file(file_name)
+  def tokenize_xml(path) do    
+    x = translate_file(path)
     |> Tokenizer.tokenize_xml()
     |> Enum.join("\n")
 
-    basename = jackfile_basename_no_prefix(file_name)
-    File.write("KO_#{basename}T.xml", x, [:append])
+    basename = jackfile_basename_no_prefix(path)
+    dirname = Path.dirname(path)
+    File.write("#{dirname}/KO_#{basename}T.xml", x, [:append])
+  end
+
+  def tokenize_xml_all(path) do
+    find_jack_files(path)
+    |> Enum.map(&tokenize_xml/1)
   end
   
-  # def translate(path) do
-  #   x = find_vm_files(path)
-  #   |> Enum.map(&translate_file/1)
-  #   |> bootstrap()
-  #   |> List.flatten
-  #   |> Enum.join("\n")
-    
-  #   basename_no_prefix = vmfile_basename_no_prefix(path)
-  #   File.write("#{basename_no_prefix}.asm", x, [:append])
-  # end
+  def jack_file?(path), do: ".jack" == Path.extname(path)
 
-  # def jack_file?(path), do: ".jack" == Path.extname(path)
-
-  # def find_files(path) do
-  #   cond do
-  #     File.dir?(path) ->
-  #       {:ok, ls_files} = File.ls(path)
-  #       ls_files
-  #       |> Enum.map(fn(fname) -> "#{path}/" <> fname end)
-  #     true ->
-  #       [path]
-  #   end
-  # end
+  def find_files(path) do
+    if File.dir?(path) do
+      {:ok, ls_files} = File.ls(path)
+      ls_files
+      |> Enum.map(fn(fname) -> "#{path}/" <> fname end)
+    else
+      [path]
+    end
+  end
   
-  # def find_vm_files(path), do: Enum.filter(find_files(path), &jack_file?/1)
+  def find_jack_files(path), do: Enum.filter(find_files(path), &jack_file?/1)
 
 end
