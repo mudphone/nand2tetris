f5fc73c5f6b30ef6388d7de7ed6154f49e634195
Kyle Oba <koba@mudphone.com>
2016-04-27 20:13:25 -1000

    Error-free compile of the Seven Jack program

--- projects/11/jack_compiler/lib/code_generation.ex
+++ projects/11/jack_compiler/lib/code_generation.ex
@@ -9,7 +9,7 @@ defmodule CodeGeneration do
     {class_code ++ code, rest1}
   end
 
-  def compile(all, symbol_table), do: {[], []}
+  def compile(_all, _symbol_table), do: {[], []}
 
   def compile_class([{:keyword, "class"},
                      {:identifier, class_name, :attr, _},
@@ -24,8 +24,8 @@ defmodule CodeGeneration do
     ++ compile_class(class_name, rest, symbol_table)
   end
 
-  def compile_class(class_name,
-        [{:symbol, "}"}], symbol_table) do
+  def compile_class(_class_name,
+        [{:symbol, "}"}], _symbol_table) do
     []
   end
 
@@ -53,21 +53,68 @@ defmodule CodeGeneration do
     ++ compile_statements(rest, symbol_table)
   end
 
-  def compile_statements([{:returnStatement, return_parsed}], symbol_table) do
-    ["return statement"]
+  def compile_statements([{:returnStatement, return_parsed} | rest], symbol_table) do
+    compile_return_statement(return_parsed, symbol_table)
+    ++ compile_statements(rest, symbol_table)
   end
 
   def compile_statements([], _symbol_table), do: []
+
+  def compile_return_statement([{:keyword, "return"},
+                               {:symbol, ";"}], _symbol_table) do
+    ["return"]
+  end
 
   def compile_do_statement([{:keyword, "do"},
                             {:identifier, class_name, :attr, _},
                             {:symbol, "."},
                             {:identifier, fn_name, :attr, _},
                             {:symbol, "("},
-                            {:expressionList, exp_parsed},
+                            {:expressionList, exp_list_parsed},
                             {:symbol, ")"},
-                            {:symbol, ";"}], symbol_table) do
-    ["do statement"]
+                            {:symbol, ";"}], _symbol_table) do
+    compile_exp_list(exp_list_parsed)
+    ++ ["call #{class_name}.#{fn_name} #{number_of_expressions(exp_list_parsed)}"]
+  end
+
+  def compile_exp_list([{:expression, exp_parsed} | more]) do
+    compile_exp(exp_parsed) ++ compile_exp_list(more)
+  end
+
+  def compile_exp_list([]), do: []
+
+  def compile_exp([{:term, term} | more]) do
+    compile_term(term) ++ compile_exp(more)
+  end
+
+  def compile_exp([{:symbol, "*"},
+                   {:term, term} | more]) do
+    compile_term(term) ++ ["call Math.multiply 2"] ++ compile_exp(more)
+  end
+
+  def compile_exp([{:symbol, "+"},
+                    {:term, term} | more]) do
+    compile_term(term) ++ ["add"] ++ compile_exp(more)
+  end
+
+  def compile_exp([]), do: []
+  
+  def compile_term([{:integerConstant, i}]) do
+    ["push constant #{i}"]
+  end
+
+  def compile_term([{:symbol, "("},
+                    {:expression, exp_parsed},
+                    {:symbol, ")"}]) do
+    compile_exp(exp_parsed)
+  end
+
+  def compile_term([]), do: []
+  
+  def number_of_expressions(exp_list) do
+    Enum.filter(exp_list, fn (tup) ->
+      List.keymember?([tup], :expression, 0)
+    end) |> length()
   end
   
   def number_of_parameters(param_list) do
