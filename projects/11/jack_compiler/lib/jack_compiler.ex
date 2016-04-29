fa809cff083231755c2defbcf7a334785a07be67
Kyle Oba <koba@mudphone.com>
2016-04-28 17:05:59 -1000

    Confirm Seven translated to VM and remove old proj

--- projects/11/jack_compiler/lib/jack_compiler.ex
+++ projects/11/jack_compiler/lib/jack_compiler.ex
@@ -1,15 +1,16 @@
 defmodule JackCompiler do
 
-  def parse_parse(path) do
+  def compile_jack_to_vm(path) do
     {parsed, symbol_t} = SyntaxAnalyzer.parse(path)
-    CodeGeneration.generate(parsed, symbol_t)
+    {vm_code, []} = CodeGeneration.compile(parsed, symbol_t)
 
+    x = Enum.join(vm_code, "\n")
     # x = CompilationEngine.to_xml(parsed, symbol_t)
     # |> Enum.join("\n")
     
-    # basename = jackfile_basename_no_prefix(path)
-    # dirname = Path.dirname(path)
-    # File.write("#{dirname}/KO_#{basename}.xml", x, [:append])
+    basename = SyntaxAnalyzer.jackfile_basename_no_prefix(path)
+    dirname = Path.dirname(path)
+    File.write("#{dirname}/KO_#{basename}.vm", x, [:append])
   end
   
 end
--- projects/11/syntax_analyzer/test/syntax_analyzer_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SyntaxAnalyzerTest do
-  use ExUnit.Case
-  doctest SyntaxAnalyzer
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
